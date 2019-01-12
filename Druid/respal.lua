6dc15462a888d3bf2d0acb04ae37420bfe45a33f
Rebecca <qwer@Me.local>
2019-01-12 01:34:40 +0100

    Started working on Gui, added isCC. Dps Toggle, Wildgrowth Customizable also changed to group.under instead of group_health~ Modified Efflorence to modifier.alt instead of IsAltKeyDown() changed a bit the readability to make it easier to read :)

--- Druid/respal.lua
+++ Druid/respal.lua
@@ -2,7 +2,7 @@
 -- Restoration Druid for 8.1 by Tacotits - 9/2018
 -- Talents: Raid=3133213 Dungeon=1133212
 -- Holding Alt = Efflorescence
--- Holding CTRL = Cleanse - mouseover
+-- Holding CTRL = Cleanse - mouseover and Battle Ressurrect if mouseover is dead
 -- Holding Shift =
 
 local dark_addon = dark_interface
@@ -24,61 +24,52 @@ SB.Drink = 274914
 SB.ReplenishmentDebuff = 252753
 SB.Regrowth = 8936
 SB.SolarWrathResto = 5176
+
+
+
 local function combat()
     if not player.alive or player.buff(SB.TravelForm).exists or player.buff(SB.Refreshment).up or player.buff(SB.Drink).up or player.channeling() then
         return
     end
+-------------
+----Fetch----
+-------------
+local wildgrowthpercent = dark_addon.settings.fetch('respal_settings_wildgrowthpercent', 80)
+local wildgrowthnumber = dark_addon.settings.fetch('respal_settings_wildgrowthnumber', 3)
 
-    --- Reticulate Splines
-    local group_health_percent = 100 * UnitHealth("player") / UnitHealthMax("player") or 0
-    local group_health = group_health_percent
-    local group_unit_count = IsInGroup() and GetNumGroupMembers() or 1
-    local damaged_units = group_health_percent < 90 and 1 or 0
-    local dead_units = 0
-    for i = 1, group_unit_count - 1 do
-        local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i);
-        local unit = IsInRaid() and "raid" .. i or "party" .. i
-        local unit_health = 100 * UnitHealth(unit) / UnitHealthMax(unit) or 0
-        if unit_health < 90 then
-            damaged_units = damaged_units + 1
-        end
-        if isDead or not online or not UnitInRange(unit) then
-            dead_units = dead_units + 1
-        else
-            group_health = group_health + unit_health
-        end
-    end
-    group_health_percent = group_health / (group_unit_count - dead_units)
-
-    -- Barkskin
+-- Barkskin
     if player.castable(SB.Barkskin) and player.health.percent < 60 then
         return cast(SB.Barkskin, player)
     end
 
-    --Trinket use
-    --healing totem trink
-    --   if GetItemCooldown(158320) == 0 and tank.health.percent < 80 then
-    --   macro('/use [help] 14; [@targettarget] 14')
-    -- end
+--[[    Trinket use
+    healing totem trink
+       if GetItemCooldown(158320) == 0 and tank.health.percent < 80 then
+       macro('/use [help] 14; [@targettarget] 14')
+     end
 
-    --first mate spyglass
-    --if GetItemCooldown(158163) == 0 and tank.health.percent < 80 then
-    --  macro('/use 13')
-    --end
-
-    -----------------------------
-    --- Health stone / Trinket  /etc
-    -----------------------------
-    --Health stone
+    first mate spyglass
+    if GetItemCooldown(158163) == 0 and tank.health.percent < 80 then
+      macro('/use 13')
+    end
+]]--
+-------------
+---Utility---
+-------------
+--Health Stone
     if player.health.percent < 30 and GetItemCount(5512) >= 1 and GetItemCooldown(5512) then
         macro('/use Healthstone')
     end
 
-
-    --modifiers
-
-    -- Maintain Efflorescence under the melee group.
-    if IsAltKeyDown() and not lastcast(SB.Efflorescence) then
+-- Innervate should be used as many times during the fight as possible. Refresh Efflorescence, cast Wild Growth, and spam Rejuvenation during Innervate.
+    if player.castable(SB.Innervate) and player.power.mana.percent < 90 then
+        return cast(SB.Innervate, player)
+    end
+-------------
+--Modifiers--
+-------------
+-- Efflorence
+    if modifier.alt and not lastcast(SB.Efflorescence) then
         return cast(SB.Efflorescence, 'ground')
     end
 
@@ -92,31 +83,9 @@ local function combat()
     end
 
 
+-- TODO: target/mouseover healing, healthstone
 
-
-    --keep dots on target
-    -- During downtime, use Moonfire, Sunfire and Solar Wrath on enemies to help with the damage.
-    if player.power.mana.percent > 55 and lowest.health.percent > 55 and tank.health.percent > 55 then
-        if target.castable(SB.Moonfire) and target.debuff(SB.Moonfire).down then
-            return cast(SB.Moonfire, target)
-        end
-        if target.castable(SB.Sunfire) and target.debuff(SB.Sunfire).down then
-            return cast(SB.Sunfire, target)
-        end
-
-
-    end
-
-
-    -- Innervate should be used as many times during the fight as possible. Refresh Efflorescence, cast Wild Growth, and spam Rejuvenation during Innervate.
-    if player.castable(SB.Innervate) and player.power.mana.percent < 90 then
-        return cast(SB.Innervate, player)
-
-    end
-
-    -- TODO: target/mouseover healing, healthstone
-
-    --- Healing Cooldowns
+--- Healing Cooldowns
     if toggle('cooldowns') then
 
         if toggle('IronBark', false) then
@@ -129,13 +98,13 @@ local function combat()
             return cast(SB.Flourish)
         end
 
-        -- Keep Lifebloom, on an active tank.
+-- Keep Lifebloom, on an active tank.
         if tank.castable(SB.Lifebloom) and tank.buff(SB.Lifebloom).down and not lastcast(SB.Lifebloom) then
             return cast(SB.Lifebloom, tank)
         end
     end
 
-    --soothe
+--soothe
     if target.castable(SB.Soothe) then
         for i = 1, 40 do
             local name, _, _, count, debuff_type, _, _, _, _, spell_id = UnitAura("target", i)
@@ -146,7 +115,7 @@ local function combat()
         end
     end
 
-    --- Decurse
+--- Decurse
     if toggle('dispell', false) then
         local dispellable_unit = group.removable('curse', 'magic', 'poison')
         if dispellable_unit and spell(SB.NaturesCure).cooldown == 0 then
@@ -159,13 +128,13 @@ local function combat()
         end
     end
 
-    --- Healing
-    -- Use Clearcasting procs to cast Regrowth on any person in the raid.
+--- Healing
+-- Use Clearcasting procs to cast Regrowth on any person in the raid.
     if player.buff(SB.Clearcasting).up and lowest.castable(SB.Regrowth) and lowest.health.percent < 80
             and not player.moving then
         return cast(SB.Regrowth, lowest)
     end
-    -- Use Cenarion Ward on cooldown.
+-- Use Cenarion Ward on cooldown.
     if talent(1, 3) and tank.castable(SB.CenarionWard) and tank.buff(SB.CenarionWard).down then
         return cast(SB.CenarionWard, tank)
     end
@@ -174,7 +143,7 @@ local function combat()
         return cast(SB.Berserking)
     end
 
-    -- Keep Rejuvenation, on the tank and on members of the group that just took damage or are about to take damage.
+-- Keep Rejuvenation, on the tank and on members of the group that just took damage or are about to take damage.
     if tank.castable(SB.Rejuvenation) and (tank.buff(SB.Rejuvenation).down or (talent(7, 2)
             and tank.buff(SB.RejuvenationGermination).down)) then
         return cast(SB.Rejuvenation, tank)
@@ -185,12 +154,11 @@ local function combat()
         return cast(SB.Rejuvenation, lowest)
     end
 
-    -- Use Wild Growth, when at least 4/6 members of the group/raid are damaged.
-    if lowest.castable(SB.WildGrowth) and not player.moving
-            and (not IsInRaid() and damaged_units >= 4 or damaged_units >= 6) then
+-- Use Wild Growth, when at least 4/6 members of the group/raid are damaged.
+
+    if lowest.castable(SB.WildGrowth) and not player.moving and group.under(wildgrowthpercent, 30, true) >= wildgrowthnumber then
         return cast(SB.WildGrowth, lowest)
     end
-
     -- Use Swiftmend on a player that just took heavy damage. If not in immediate danger, use Rejuvenation first.
     if lowest.castable(SB.Swiftmend)
             and (lowest.buff(SB.Rejuvenation).up and lowest.health.percent <= 75 or lowest.health.percent <= 50) then
@@ -200,7 +168,7 @@ local function combat()
         return cast(SB.Swiftmend, tank)
     end
 
-    -- Use Regrowth as an emergency heal.
+-- Use Regrowth as an emergency heal.
     if not IsInRaid() then
         if tank.castable(SB.Regrowth) and not player.moving and tank.health.percent <= 70 then
             return cast(SB.Regrowth, tank)
@@ -217,14 +185,28 @@ local function combat()
         end
     end
 
+-------------
+-----DPS-----
+-------------
+--keep dots on target
+--During downtime, use Moonfire, Sunfire and Solar Wrath on enemies to help with the damage.
+    if player.power.mana.percent > 55 and lowest.health.percent > 55 and tank.health.percent > 55 and toggle('dps', false) and not isCC("target") then
+        if target.castable(SB.Moonfire) and target.debuff(SB.Moonfire).down then
+            return cast(SB.Moonfire, target)
+        end
+        if target.castable(SB.Sunfire) and target.debuff(SB.Sunfire).down then
+            return cast(SB.Sunfire, target)
+        end
+    
+
     if target.castable(SB.SolarWrathResto) and not player.moving then
         return cast(SB.SolarWrathResto, target)
     end
     if target.castable(SB.Moonfire) and player.moving then
         return cast(SB.Moonfire, target)
     end
-end -- end combat()
-
+    end
+end 
 local function resting()
 
     local outdoor = IsOutdoors()
@@ -262,23 +244,23 @@ local function resting()
         return cast(SB.Efflorescence, 'ground')
     end
 
-    -- Keep Lifebloom, on an active tank.
+-- Keep Lifebloom, on an active tank.
     if IsInGroup() and tank.castable(SB.Lifebloom) and tank.buff(SB.Lifebloom).down and not lastcast(SB.Lifebloom) then
         return cast(SB.Lifebloom, tank)
     end
-    -- Swiftmend
+-- Swiftmend
     if player.castable(SB.Swiftmend) and player.health.percent < 50 then
         return cast(SB.Swiftmend, player)
     end
-    -- Rejuvenation
+-- Rejuvenation
     if player.castable(SB.Rejuvenation) and not player.buff(SB.Rejuvenation).up and player.health.percent < 75 then
         return cast(SB.Rejuvenation, player)
     end
-    -- Regrowth
+-- Regrowth
     if player.castable(SB.Regrowth) and ((player.health.percent < 75 and not player.buff(SB.Regrowth).up) or player.health.percent < 30) then
         return cast(SB.Regrowth, player)
     end
-    -- Barkskin
+-- Barkskin
     if player.castable(SB.Barkskin) and player.health.percent < 50 then
         return cast(SB.Barkskin, player)
     end
@@ -301,7 +283,13 @@ local function interface()
     template = {
       { type = 'header', text = 'Restoration Pal - Settings', align= 'center' },
       { type = 'rule' },
-     
+      { type = 'text', text = 'Class Settings' },
+      { type = 'rule' },
+      { type = 'text', text = 'Wild Growth Settings' },
+      { key = 'wildgrowthpercent', type = 'spinner', text = 'Wild Growth', desc = 'Health Percent to Cast At', default = 80,  min = 1, max = 100, step = 5 },
+      { key = 'wildgrowthnumber', type = 'spinner', text = 'Wild Growth Targets', desc = 'Minimum Wild Growth Targets', default = 3, min = 1, max = 40, step = 1 },
+
+
     }
   }  
   configWindow = dark_addon.interface.builder.buildGUI(settings)
@@ -334,6 +322,20 @@ local function interface()
             color2 = dark_addon.interface.color.ratio(dark_addon.interface.color.red, 0.5)
         }
     })
+    dark_addon.interface.buttons.add_toggle({
+        name = 'dps',
+        label = 'Use Damage Spells',
+        on = {
+            label = 'DPS ON',
+            color = dark_addon.interface.color.orange,
+            color2 = dark_addon.interface.color.ratio(dark_addon.interface.color.dark_orange, 0.7)
+        },
+        off = {
+            label = 'DPS OFF',
+            color = dark_addon.interface.color.red,
+            color2 = dark_addon.interface.color.ratio(dark_addon.interface.color.red, 0.5)
+        }
+        })
     dark_addon.interface.buttons.add_toggle({
         name = 'dispell',
         label = 'dispell',
