1cce597f16b9d7cb74c2f525a94df020ebe31bcd
Rex <>
2019-03-10 19:59:11 +0000

    Update outrogpal.lua
    Significantly reduced code as there was duplication across ST and MT rotations. Added toggle for Blade Flurry TTTD check.

--- Rogue/outrogpal.lua
+++ Rogue/outrogpal.lua
@@ -4,7 +4,6 @@ local SB = dark_addon.rotation.spellbooks.rogue
 -- To Do
 
 -- Spells
-
 SB.Vanish = 1856
 SB.Ambush = 8676
 SB.CheapShot = 1833
@@ -97,8 +96,8 @@ local function combat()
     end
 
     --Interrupts
-        --Define random number for interrupt
-        local intpercent = math.random(intpercentlow,intpercenthigh)
+    --Define random number for interrupt
+    local intpercent = math.random(intpercentlow,intpercenthigh)
 
         -- Kick
         if toggle('interrupts', false) and castable(SB.Kick) and -spell(SB.Kick) == 0 and target.interrupt(intpercent, false) and target.distance < 8 then
@@ -124,19 +123,19 @@ local function combat()
     --Cloak of Shadows
     local dispellable_unit = player.removable('disease', 'magic', 'poison')
 
-    if castable(SB.CloakofShadows) and -spell(SB.CloakofShadows) == 0 and -player.health <= COSHealth and dispellable_unit then
-        return cast(SB.CloakofShadows, 'player')
-    end
-      
-    --Feint
-    if castable(SB.Feint) and -spell(SB.Feint) == 0 and -player.health <= FHealth and player.power.energy.actual >= 35 then
-        return cast(SB.Feint, 'player')
-    end
+        if castable(SB.CloakofShadows) and -spell(SB.CloakofShadows) == 0 and -player.health <= COSHealth and dispellable_unit then
+            return cast(SB.CloakofShadows, 'player')
+        end
+          
+        --Feint
+        if castable(SB.Feint) and -spell(SB.Feint) == 0 and -player.health <= FHealth and player.power.energy.actual >= 35 then
+            return cast(SB.Feint, 'player')
+        end
 
-    --Riposte
-    if castable(SB.Riposte) and -spell(SB.Riposte) == 0 and -player.health <= RHealth then
-        return cast(SB.Riposte, 'player')
-    end
+        --Riposte
+        if castable(SB.Riposte) and -spell(SB.Riposte) == 0 and -player.health <= RHealth then
+            return cast(SB.Riposte, 'player')
+        end
 
     --Vanish
 
@@ -145,8 +144,17 @@ local function combat()
         return cast(SB.CrimsonVial, 'player')
     end
 
---Single Target Rotation
-if enemyCount == 1 then
+    --Single Target and AoE Combined Rotation
+    --Cast Blade Flurry if there are 2+ targets.
+    if castable(SB.BladeFlurry) and -spell(SB.BladeFlurry) == 0 and enemyCount >= 2 and (toggle('ttdtoggle', true) or (target.time_to_die > 6 and toggle('ttdtoggle', false))) then
+      return cast(SB.BladeFlurry, 'target')
+    end
+
+    --Cast Pistol Shot with Deadshot proc as quick as you can without overcapping
+    if castable(SB.PistolShot) and -spell(SB.PistolShot) == 0 and player.buff(SB.Deadshot).up 
+    and (player.power.combopoints.actual <= 4 or (player.power.combopoints.actual <= 3 and talent(1,1))) then
+      return cast(SB.PistolShot, 'target')
+    end        
 
     if not deadshottrait and not aceupyoursleevetrait then
         --Cast 4-5 Combo Point Roll the Bones (see dedicated Roll the Bones section for details).
@@ -202,89 +210,14 @@ if enemyCount == 1 then
 
     --Cast Pistol Shot if you have an Opportunity proc and you have 4 or less Combo Points (and will not Energy cap during the global cooldown).
     if castable(SB.PistolShot) and -spell(SB.PistolShot) == 0 and player.buff(SB.Opportunity).up 
-    and (player.power.combopoints.actual <= 4 or (player.power.combopoints.actual <= 4 and talent(1,1))) then
+    and (player.power.combopoints.actual <= 4 or (player.power.combopoints.actual <= 3 and talent(1,1))) then
       return cast(SB.PistolShot, 'target')
     end
 
     --Cast Sinister Strike to generate Combo Points.
     if castable(SB.SinisterStrike) and -spell(SB.SinisterStrike) == 0 then
       return cast(SB.SinisterStrike, 'target')
-    end
-end
-
---Multi Target Rotation
-if enemyCount >= 2 then
-
-    --Cast Blade Flurry if there are 2+ targets.
-    if castable(SB.BladeFlurry) and -spell(SB.BladeFlurry) == 0 and target.time_to_die > 6 then
-      return cast(SB.BladeFlurry, 'target')
-    end
-
-    if not deadshottrait and not aceupyoursleevetrait then
-        --Cast 4-5 Combo Point Roll the Bones (see dedicated Roll the Bones section for details).
-        if castable(SB.RolltheBones) and -spell(SB.RolltheBones) == 0 and player.power.combopoints.actual >= 4 and not talent(6,3) 
-        and ((rtbcount < 2 and player.buff(SB.LoadedDice).up) or (rtbcount < 2 and player.buff(SB.GrandMelee).down and player.buff(SB.RuthlessPrecision).down)) then
-          return cast(SB.RolltheBones, 'target')
-        end
-    end     
-    
-    if deadshottrait or aceupyoursleevetrait then
-        --Cast 4-5 Combo Point Roll the Bones (see dedicated Roll the Bones section for details).
-        if castable(SB.RolltheBones) and -spell(SB.RolltheBones) == 0 and player.power.combopoints.actual >= 4 and not talent(6,3) 
-        and ((rtbcount < 2 and player.buff(SB.LoadedDice).up) or (rtbcount < 2 and player.buff(SB.RuthlessPrecision).down)) then
-          return cast(SB.RolltheBones, 'target')
-        end
-    end
-
-    --Cast Ghostly Strike (if talented) on cooldown, in sync with Blade Flurry unless you will over-cap Combo Points from it.
-    if castable(SB.GhostlyStrike) and -spell(SB.GhostlyStrike) == 0 and player.power.combopoints.actual <= 4 
-    and player.buff(SB.BladeFlurry).up and talent(1,3) then
-      return cast(SB.GhostlyStrike, 'target')
-    end
-
-    --Cast Killing Spree / Blade Rush on cooldown; if Adrenaline Rush is active, delay to prevent over-capping on energy, only if Blade Flurry is currently active.
-    if castable(SB.BladeRush) and -spell(SB.BladeRush) == 0 and talent(7,2) and player.buff(SB.AdrenalineRush).down 
-    and player.buff(SB.BladeFlurry).up then
-      return cast(SB.BladeRush, 'target')
-    end
-
-    --Cast Killing Spree / Blade Rush on cooldown; if Adrenaline Rush is active, only if Blade Flurry is currently active.
-    if castable(SB.KillingSpree) and -spell(SB.KillingSpree) == 0 and talent(7,3) and player.buff(SB.BladeFlurry).up then
-      return cast(SB.KillingSpree, 'target')
     end        
-
-    --Activate Adrenaline Rush 
-    if castable(SB.AdrenalineRush) and -spell(SB.AdrenalineRush) == 0 and toggle('cooldowns', false) then
-      return cast(SB.AdrenalineRush, 'target')
-    end
-
-    --Cast Marked for Death (if talented) if you have 0-1 Combo Points.
-    if castable(SB.MarkedforDeath) and -spell(SB.MarkedforDeath) == 0 and player.power.combopoints.actual <= 1 and talent(3,3) then
-      return cast(SB.MarkedforDeath, 'target')
-    end
-
-    --Cast Between the Eyes at 5 Combo Points if you have a Ruthless Precision proc, or Ace Up Your Sleeve , or Deadshot
-    if castable(SB.BetweentheEyes) and -spell(SB.BetweentheEyes) == 0 and player.power.combopoints.actual >= 5 
-    and (player.buff(SB.RuthlessPrecision).up or aceupyoursleevetrait or deadshottrait) then
-      return cast(SB.BetweentheEyes, 'target')
-    end
-
-    --Cast Dispatch at 5 Combo Points.
-    if castable(SB.Dispatch) and -spell(SB.Dispatch) == 0 and player.power.combopoints.actual >= 5 then
-      return cast(SB.Dispatch, 'target')
-    end
-
-    --Cast Pistol Shot if you have an Opportunity proc and you have 4 or less Combo Points (and will not Energy cap during the global cooldown).
-    if castable(SB.PistolShot) and -spell(SB.PistolShot) == 0 and player.buff(SB.Opportunity).up 
-    and (player.power.combopoints.actual <= 4 or (player.power.combopoints.actual <= 4 and talent(1,1))) then
-      return cast(SB.PistolShot, 'target')
-    end
-
-    --Cast Sinister Strike to generate Combo Points.
-    if castable(SB.SinisterStrike) and -spell(SB.SinisterStrike) == 0 then
-      return cast(SB.SinisterStrike, 'target')
-    end
-end         
 
 end
 end
@@ -314,7 +247,8 @@ local function interface()
             { type = 'header', text = "            Rex's Outlaw Rogue Settings" },
             { type = 'text', text = 'Everything on the screen is LIVE.  As you make changes, they are being fed to the engine' },
             { type = 'text', text = 'Suggested Talents - 2 3 1 2 3 2 2' },
-            { type = 'text', text = 'Adrenaline Rush is managed by the Cooldowns toggle' },            
+            { type = 'text', text = 'Adrenaline Rush is managed by the Cooldowns toggle' }, 
+            { type = 'text', text = 'Blade Flurry has a Time-To-Die check to not waste it on low health mobs - this can be toggled on and off'},                       
             { type = 'text', text = 'If you want automatic AOE then please remember to turn on EnemyNamePlates in WoW (V key)' },
             { type = 'rule' },
             { type = 'text', text = 'Interrupt Settings' },
@@ -329,6 +263,21 @@ local function interface()
     }
 
     configWindow = dark_addon.interface.builder.buildGUI(settings)
+
+    dark_addon.interface.buttons.add_toggle({
+    name = 'ttdtoggle',
+    label = 'BF TTD',
+    on = {
+      label = 'BF TTD On',
+      color = dark_addon.interface.color.yellow,
+      color2 = dark_addon.interface.color.yellow
+    },
+    off = {
+      label = 'BF TTD Off',
+      color = dark_addon.interface.color.grey,
+      color2 = dark_addon.interface.color.dark_grey
+    }
+    })
 
     dark_addon.interface.buttons.add_toggle({
         name = 'settings',
