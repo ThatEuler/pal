ad5f89aab60fe8a49ed671101aea9032aa671972
Gauss Euler <thateuler@gmail.com>
2019-05-12 11:57:20 -0400

    add remaining < lvl20 spells

--- Warrior/myprot.lua
+++ Warrior/myprot.lua
@@ -1,12 +1,8 @@
 local dark_addon = dark_interface
-
--- replace dark_addon.rotation.spellbooks.class
--- with the appropriate value for your class
--- ex: dark_addon.rotation.spellbooks.mage
 local SB = dark_addon.rotation.spellbooks.warrior
 SB.VictoryRushBuff = 32216
 local EternalEmburfuryTalisman = 122667
-
+local enemies_visible = {}
 local race = UnitRace("player")
 
 local function combat()
@@ -39,25 +35,87 @@ local function combat()
     return cast(SB.VictoryRush, target)
   end
 
+  ---------------
+  -- Auto Taunt
+  -- Aggro all the enemies
+  -- This code isn't well tested.
+  -- It seems that UnitThreatSituation() always returns nil
+  -- TODO if this is slow in raids.
+  -- check healers for any agro
+  --  check al mos against the healer
+  -- check all dps for any agro
+  --  check all mobs against the healer
+  -- also. put it in the gcd.
+  ---------------
+  if -spell(SB.Taunt) == 0 or -spell(SB.HeroicThrow) then
+    local members = GetNumGroupMembers()
+    local group_type = GroupType()
+    -- for each party member, and each enemy, check if
+    -- they are tanking. if yes, taunt the enemy.
+    if group_type ~= 'solo' then
+      local member = group_type .. i
+      for i = 1, (members - 1) do
+        if UnitGroupRolesAssigned(member) ~= "TANK" then
+          for unitID, unitName in pairs(enemies_visible) do
+            if UnitExists(unitID) and UnitAffectingCombat(unitID) then
+              isTanking = UnitThreatSituation(member, unitID)
+              if isTanking == 0 then
+                print((UnitName(member)), " is tanking with ", unitName)
+                if -spell(SB.HeroicThrow) and IsSpellInRange('Heroic Throw', unitID) then
+                  return case(SB.HeroicThrow, unitID)
+                end
+                if -spell(SB.Taunt) and IsSpellInRange('Taunt', unitID) then
+                  return cast(SB.Taunt, unitID)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
   ---------
   -- Combat
   ---------
 
   -- Charge
-  if target.castable(SB.Charge)
-     and -spell(SB.Charge) == 0 then
+  if castable(SB.Charge)
+  and target.castable(SB.Charge)
+  and -spell(SB.Charge) == 0
+  then
     return cast(SB.Charge, target)
   end
 
+  -- Shield Block
+  if castable(SB.ShieldBlock)
+  and -spell(SB.ShieldBlock) == 0
+  and -power.rage >= 30
+  and target.time_to_die > 6
+  and player.buff(SB.ShieldBlockBuff).down
+  then
+    return cast(SB.ShieldBlock)
+  end
+
+  -- ThunderClap
+  if castable(SB.ThunderClap)
+  and -spell(SB.ThunderClap) == 0 then
+    return cast(SB.ThunderClap)
+  end
+
   -- Shield Slam
-  if target.castable(SB.ShieldSlam)
-     and -spell(SB.ShieldSlam) == 0 then
+  if castable(SB.ShieldSlam)
+  and target.castable(SB.ShieldSlam)
+  and -spell(SB.ShieldSlam) == 0
+  then
     return cast(SB.ShieldSlam, target)
   end
 
   -- Devastate
-  if target.castable(SB.Devastate)
-     and -spell(SB.Devastate) == 0 then
+  if castable(SB.Devastate)
+  and target.castable(SB.Devastate)
+  and -spell(SB.Devastate) == 0
+  then
     return cast(SB.Devastate, target)
   end
 
@@ -76,6 +134,7 @@ local function resting()
   end
 end
 
+
 local function interface()
   dark_addon.interface.buttons.add_toggle(
     {
@@ -93,10 +152,43 @@ local function interface()
   )
 end
 
+------
+-- Maintain a list of enemies
+-- make sure nameplates are always on
+------
+local function add_enemy(unitID)
+  if not enemies_visible[unitID] and not UnitIsFriend("player", unitID) then
+    local name, _ = UnitName(unitID)
+    --print("adding: ", unitID, " name: ", name)
+    enemies_visible[unitID] = dark_addon.environment.conditions.unit(unitID)
+  end
+end
+
+local function remove_enemy(unitID)
+  if enemies_visible[unitID] then
+    local name, _ = UnitName(unitID)
+    --print("removing: ", unitID, " name: ", name)
+    enemies_visible[unitID] = nil
+  end
+end
+
+dark_addon.event.register("NAME_PLATE_UNIT_ADDED", function(...)
+  return add_enemy(...)
+end)
+
+dark_addon.event.register("NAME_PLATE_UNIT_REMOVED", function(...)
+  return remove_enemy(...)
+end)
+
+
+-----------
+-- Register
+-----------
+
 dark_addon.rotation.register({
   spec = dark_addon.rotation.classes.warrior.protection,
   name = 'myprot',
-  label = 'My Awesome Rotation',
+  label = 'Prot Warrior <= level 20',
   combat = combat,
   resting = resting,
   interface = interface
